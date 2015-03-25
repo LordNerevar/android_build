04378e3ac32d35f8fd435bd1b9801ead35f3a089
Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
2015-03-25 17:15:29 +0100

    Don't use -Wl,--fix-cortex-a8 for Cortex A7, A9, A15, Krait or Denver
    -Wl,--fix-cortex-a8 is a workaround for an Erratum in Cortex-A8
    processors. It slightly increases code size and decreases performance,
    and there's no point in using it on non-A8 CPUs.
    
    Instead of forcing it unconditionally, use it when targeting
    Cortex-A8 or generic armv7-a (which might or might not be A8).
    
    Change-Id: I77c674eafda4089267df2dd5649e9fdb60c428ca
    Signed-off-by: Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
    Signed-off-by: Maxime Poulain <maxime.poulain@student.uclouvain.be>

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -8,32 +8,35 @@ ARCH_ARM_HAVE_NEON              := true
 
 ifeq ($(TARGET_CPU_VARIANT),$(filter $(TARGET_CPU_VARIANT),cortex-a15 krait))
 	arch_variant_cflags := -mcpu=cortex-a15 -mfpu=neon-vfpv4
+
+	# Fake an ARM compiler flag as these processors support LPAE which GCC/clang
+	# don't advertise.
+	arch_variant_cflags += -D__ARM_FEATURE_LPAE=1
+	arch_variant_ldflags := -Wl,--no-fix-cortex-a8
 else
 ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a9)
 	arch_variant_cflags := -mcpu=cortex-a9 -mfpu=neon
+	arch_variant_ldflags := -Wl,--no-fix-cortex-a8
 else
-ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a8)
+ifeq ($(TARGET_CPU_VARIANT),$(filter $(TARGET_CPU_VARIANT),cortex-a8 scorpion))
 	arch_variant_cflags := -mcpu=cortex-a8 -mfpu=neon
+	arch_variant_ldflags := -Wl,--fix-cortex-a8
 else
 ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a7)
 	arch_variant_cflags := -mcpu=cortex-a7 -mfpu=neon-vfpv4
+	arch_variant_ldflags := -Wl,--no-fix-cortex-a8
 else
 ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a5)
 	arch_variant_cflags := -mcpu=cortex-a5 -mfpu=neon-vfpv4
-else
-ifeq ($(strip $(TARGET_CPU_VARIANT)),scorpion)
-	arch_variant_cflags := -mcpu=cortex-a8 -mfpu=neon
+	arch_variant_ldflags := -Wl,--no-fix-cortex-a8
 else
 	arch_variant_cflags := -march=armv7-a -mfpu=neon
-endif
+	# Generic ARM might be a Cortex A8 -- better safe than sorry
+	arch_variant_ldflags := -Wl,--fix-cortex-a8
 endif
 endif
 endif
 endif
 endif
 
-arch_variant_cflags += \
-    -mfloat-abi=softfp
-
-arch_variant_ldflags := \
-	-Wl,--fix-cortex-a8
+arch_variant_cflags += -mfloat-abi=softfp
